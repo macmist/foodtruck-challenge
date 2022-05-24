d4bbef64a125306adc27659284f3e055f655838d
Quentin MUSY <quentin.musy@gmail.com>
2022-05-24 15:40:21 +0100

    fixed wrong sort order

--- app/dao/FoodTruck.js
+++ app/dao/FoodTruck.js
@@ -15,13 +15,14 @@ const FoodTruckDao = {
         return FoodTruck.insertMany(trucks)
     },
     getClosest: (latitude, longitude) => {
+        console.log(latitude, longitude)
         return FoodTruck.aggregate([{
             $geoNear: {
                 distanceField: 'distance',
                 near: { type: "Point", coordinates: [longitude, latitude]},
                 spherical: true
             }},
-            {$sort: {distance: -1}},
+            {$sort: {distance: 1}},
             {$project:  {distance: 1, facilityType: 1, description:  1, items: 1, schedule: 1}}
         ]).exec()
     }
